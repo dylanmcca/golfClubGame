cc02a6da6160fcad55b151e9dd8d72f674d6e3fa
dylanmcca <dylanmcca@gmail.com>
2025-09-20 15:56:14 +0100

    use JS to create math function for distance auto generation on start of game & after each shot

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// distance generator to run as soon as page loads 
+document.addEventListener("DOMContentLoaded", function() {
+    runGame();
+});
+// create and show a random distance for game
+function runGame() {
+    const distance = randInt(1, 500);
+    document.getElementById("distance").textContent = distance + " meters";
+}
+
+// generate randon distance using Math Random & Math floor methods
+function randInt(min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
